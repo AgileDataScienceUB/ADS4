795293f7c66347ab672159095ae5d9339af96e69
Maria Aragon Perez <maragon@icscc.loc>
2017-11-17 12:56:06 +0100

    updating views

--- view/js/init.js
+++ view/js/init.js
@@ -2,6 +2,9 @@
   $(function(){
 
     $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+    $('.modal').modal();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
