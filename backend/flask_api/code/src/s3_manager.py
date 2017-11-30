3470c4735fcd867cb71d5dbf4dc0474f2a98d68c
Albert Companys <companys.albert@gmail.com>
2017-11-30 16:18:11 +0100

    Improve s3_manager library

--- backend/flask_api/code/src/s3_manager.py
+++ backend/flask_api/code/src/s3_manager.py
@@ -2,21 +2,40 @@ import sys
 import boto3
 import os
 
-def get_s3_resource():
-    return boto3.resource('s3')
+def get_s3_client():
+    return boto3.client(
+            "s3",
+            aws_access_key_id=os.environ.get("AWS_ACCES_KEY_ID"),
+            aws_secret_access_key=os.environ.get("AWS_SECRET_ACCES_KEY")
+            )
 
-def upload_file(filename, bucket, path=""):
-    s3_resource = get_s3_resource()
+def upload_file(filename, bucket, acl="public-read"):
+
+    s3_client = get_s3_client()
     filename = filename.split("/")[-1]
     filename = filename.split("\\")[-1]
-    s3_resource.Bucket(bucket).upload_file(filename, path + filename)
+    file_object = open(filename, 'r')
+    try:
+        s3_client.upload_fileobj(
+            file_object,
+            bucket,
+            filename,
+            ExtraArgs={
+                "ACL":acl,
+                "ContentType":file_object.content_type
+            }
 
+        )
+    except Exception as e:
+        print("Something Happened:", e)
+        return e
+    
 def download_file(key, bucket, local_path=None):
     if local_path is None:
         filename = key.split("/")[-1]
         filename = filename.split("\\")[-1]
         local_path = os.path.abspath(os.sep) + filename
-    s3_resource = get_s3_resource()
+    s3_resource = get_s3_client()
     s3_resource.Bucket(bucket).download_file(key, local_path)
 
 if __name__ == '__main__':
@@ -26,6 +45,6 @@ if __name__ == '__main__':
         else:
             path=""
 
-        upload_file(sys.argv[1], sys.argv[2], path=path)
+        upload_file(sys.argv[1], sys.argv[2])
     except Exception:
         print("Usage: \npython s3upload.py file bucket [path]")
