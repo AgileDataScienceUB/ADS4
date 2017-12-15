4d1e86128e7b0f62900fb05f99cdece0787f06fc
María Aragón <maragon@idiapjgol.info>
2017-12-15 14:08:20 +0100

    Frontend changes, visualizations

--- view/js/init.js
+++ view/js/init.js
@@ -8,3 +8,15 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+
+
+function check(form) { /*function to check userid & password*/
+	/*the following code checkes whether the entered userid and password are matching*/
+	if(form.email.value == "eloi@focus2hit.com" && form.password.value == "eloi@focus2hit.com") {
+ 		window.open('./uploadData.html')/*opens the target page while Id & password matches*/
+
+	}else {
+		window.alert("Wrong credentials, try again!");
+	}
+}
