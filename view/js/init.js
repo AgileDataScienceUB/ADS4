38eb53c5b10519ea4c9efb4ed7133a74e957ac22
Maria Aragon Perez <maragon@icscc.loc>
2017-12-20 15:11:06 +0100

    playing with backend

--- view/js/init.js
+++ view/js/init.js
@@ -20,3 +20,46 @@ function check(form) { /*function to check userid & password*/
 		window.alert("Wrong credentials, try again!");
 	}
 }
+
+
+//actual handler
+$("#submitbutton").on("click", function(){
+
+	//arguments
+	var myfile = $("#csvfile")[0].files[0];
+
+	if(!myfile){
+		alert("Please select a file");
+		return;
+	}
+
+	//disable the button during upload
+	$("#submitbutton").attr("disabled", "disabled");
+	$("#loader").attr("active", "active");
+	
+
+
+	var form = new FormData();
+	form.append("file", myfile);
+
+	var settings = {
+			"async": true,
+			"crossDomain": true,
+			"url": "http://34.243.4.122:3031/create-predictor",
+			"method": "POST",
+			"headers": {
+			"cache-control": "no-cache",
+			"postman-token": "890f1954-eb41-8e3d-90a0-7e6f5a284141"
+		},
+		"processData": false,
+		"contentType": false,
+		"mimeType": "multipart/form-data",
+		"data": form
+	}
+
+
+	$.ajax(settings).done(function (response) {
+		console.log(response);
+	});
+
+});
