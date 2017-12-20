5aa7a3a26cb3d6d8961dbc2d514c507223233adc
Arnau <arnauescapa@gmail.com>
2017-12-20 20:42:38 +0100

    plots B ajustament amb diccionaris en comptes de txt

--- data_analysis/plotsB.py
+++ data_analysis/plotsB.py
@@ -1,45 +1,35 @@
 import numpy as np
 import pandas as pd
-import os, errno
-import pickle as pk
 
-def generate_plotsB_files(test_data_path,predictions_path):
-    "Generates a directory ./PlotsB with one txt file for each B plot. "
-    try:
-        os.makedirs("./PlotsB")
-    except OSError as e:
-        if e.errno != errno.EEXIST:
-            raise
+def generate_plotsB_files(test_data_path,pred):
+    "Generates dict for each B plot. "
+
     df=pd.read_csv(test_data_path)
-    pred=pk.load( open( predictions_path, "rb" ) ) ###pred has to be an array with the predictions ### FALTA TEST NIDEA d'on es fa aquest pickle ni on es guarda
+        
+    dict_B1=plotB1(df,pred)
+    dict_B2=plotB2(df,pred)
+    dict_B3=plotB3(df,pred)
+    dict_B4=plotB4(pred)
+    dict_B5=plotB5(df,pred)
     
-    pathB1=plotB1(df,pred)
-    pathB2=plotB2(df,pred)
-    pathB3=plotB3(df,pred)
-    pathB4=plotB4(pred)
-    pathB5=plotB5(df,pred)
-    
-    return (pathB1,pathB2,pathB3,pathB4,pathB5)
+    return (dict_B1,dict_B2,dict_B3,dict_B4,dict_B5)
 
 def plotB1(df,pred):
     "Returns path of a txt file with info about Scatter plot. "
     "First row contains strings with title and layers X and Y(seperated by comas). Then each row is a point (x,y) of a scatter plot"
     
     n=df.shape[0]
-    title= "Probability to leave vs lenght of service"
+    title= "Probability to leave vs salary"
     
     salary_lavel= "MonthlyIncome" ##!!
     
     layerX= "Salary"
     layerY= "Probability to leave "
     x=df[salary_lavel].values
-
-    file=open('./PlotsB/plotB1.txt','w')
-    file.write('{}, {}, {} \n'.format(title, layerX, layerY))
-    for i in range(n):     
-        file.write('{} {} \n'.format(x[i], pred[i]))
-    file.close()
-    return('./PlotsB/plotB1.txt')
+    dict = {"plot-name": title}
+    dict["layer-x"]=layerX; dict["layer-y"]=layerY;
+    dict["values"]=[x.tolist(),pred.tolist()]
+    return(dict)
 
 def plotB2(df,pred):
     "Returns path of a txt file with info about Column plot. "
@@ -53,17 +43,16 @@ def plotB2(df,pred):
     title="Probability of leaving respect job roles"
     layerX= "Job role"
     layerY= "Mean by role of lenght of service"
-    file=open('./PlotsB/plotB2.txt','w')
-    file.write('{}, {}, {} \n'.format(title, layerX, layerY))    
+    dict = {"plot-name": title}
+    dict["layer-x"]=layerX; dict["layer-y"]=layerY
 
     roles=df[job_role_lavel].values
     unique_roles=np.unique(roles)
     for i in unique_roles:
         inx= i==roles
         y=np.mean(pred[inx])
-        file.write('{} {} \n'.format(i, y))
-    file.close()
-    return('./PlotsB/plotB2.txt')
+        dict[i]=y
+    return(dict)
 
 def plotB3(df,pred):
     "Returns path of a txt file with info about Column plot. "
@@ -76,11 +65,11 @@ def plotB3(df,pred):
     
     title="Probability of leaving respect age"
     layerX= "Age group"
-    layerY= "Predicted mean by age group"
+    layerY= "Percentatge of employees with prediction higher than 80%"
     layersX=["Young(<30)", "Medium(30-50)","Old(>50)"]
    
-    file=open('./PlotsB/plotB3.txt','w')
-    file.write('{}, {}, {} \n'.format(title, layerX, layerY))    
+    dict = {"plot-name": title}
+    dict["layer-x"]=layerX; dict["layer-y"]=layerY   
     ages=df[age_lavel].values
     y=np.empty(3)
     
@@ -97,9 +86,9 @@ def plotB3(df,pred):
     y[2]=x/n
     
     for i in range(3) :        
-        file.write('{} {}\n'.format(layersX[i],y[i]))
-    file.close()
-    return('./PlotsB/plotB3.txt')
+        dict[layersX[i]]=y[i]
+
+    return(dict)
 
 def plotB4(pred):
     "Returns path of a txt file with info about Column plot(histrogram). "
@@ -110,12 +99,12 @@ def plotB4(pred):
     layerX= "Proabability values"
     layerY= "Number of predicted probabilities"
     y,x=np.histogram(pred,bins=20)
-    file=open('./PlotsB/plotB4.txt','w')
-    file.write('{}, {}, {} \n'.format(title, layerX, layerY))
-    for i in range(len(y)):  
-        file.write('{} {} \n'.format(x[i], y[i]))
-    file.close()
-    return('./PlotsB/plotB4.txt')
+    
+    dict = {"plot-name": title}
+    dict["layer-x"]=layerX; dict["layer-y"]=layerY
+    dict["values"]=[y.tolist(),x.tolist()]
+    
+    return(dict)
 
 def plotB5(df,pred):
     "Returns path of a txt file with info about Line plot. "
@@ -127,22 +116,22 @@ def plotB5(df,pred):
     ##!!
     
     title="Probability of leaving respect age group"
-    layerX= "Lenght of service"
+    layerX= "Lenght of service (years)"
     layerY= "Probability of leaving"
    
-    file=open('./PlotsB/plotB5.txt','w')
-    file.write('{}, {}, {} \n'.format(title, layerX, layerY))   
-    
+    dict = {"plot-name": title}
+    dict["layer-x"]=layerX; dict["layer-y"]=layerY
+    values=[]
     LOS=df[LOS_lavel].values
     Dom=LOS.max()-LOS.min()  #domain
     num_points=30; I=Dom/num_points
-
+    X=[]; Y=[]
     for i in range(num_points):
         ind= np.where( (i*I<=LOS) & (LOS<(i+1)*I),1,0)
         y=pred[ind].mean()
         if (sum(ind>0)):
-            file.write('{} {}\n'.format((i+0.5)*I,y))
-    
-    file.close()
-    return('./PlotsB/plotB5.txt')
+            X.append((i+0.5)*I)
+            Y.append(y) 
+    dict["values"]=[X,Y]
+    return(dict)
 
